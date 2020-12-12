aa50d2d74e49eedd16c9a5a60117d755477d5fd5
eugustavo <gustavo.ifto@gmail.com>
2020-12-12 01:10:57 -0300

    chore: Create JWT session

---
+++ database/migrations/1503250034280_token.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class TokensSchema extends Schema {
+  up() {
+    this.create('tokens', table => {
+      table
+        .uuid('id')
+        .unique()
+        .defaultTo(this.db.raw('public.gen_random_uuid()'))
+        .primary();
+      table.uuid('user_id').unsigned().references('id').inTable('users');
+      table.string('token', 255).notNullable().unique().index();
+      table.string('type', 80).notNullable();
+      table.boolean('is_revoked').defaultTo(false);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('tokens');
+  }
+}
+
+module.exports = TokensSchema;
