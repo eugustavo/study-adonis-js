8fa2e305e667cb158468a8c442f49759a513b99a
eugustavo <gustavo.ifto@gmail.com>
2020-12-12 01:09:40 -0300

    chore: Create user methods

---
+++ database/migrations/1503250034279_user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class UserSchema extends Schema {
+  up() {
+    this.create('users', table => {
+      table
+        .uuid('id')
+        .unique()
+        .defaultTo(this.db.raw('public.gen_random_uuid()'))
+        .primary();
+      table.string('username', 80).notNullable().unique();
+      table.string('email', 254).notNullable().unique();
+      table.string('password', 60).notNullable();
+      table.string('token');
+      table.timestamp('token_created_at');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('users');
+  }
+}
+
+module.exports = UserSchema;
